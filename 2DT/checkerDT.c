db38ce2c3963b77a97baa504a48b149773d614ec
Ariella Pressner <ap0760@princeton.edu>
2024-04-03 21:09:40 -0400

    only return if false

--- 2DT/checkerDT.c
+++ 2DT/checkerDT.c
@@ -98,6 +98,7 @@ static boolean CheckerDT_bNotInitialized(Node_T oNRoot, size_t ulCount)
       fprintf(stderr, "Not initialized, but root is not NULL\n");
       return FALSE;
    }
+   else return TRUE;
 }
 
 /* see checkerDT.h for specification */
