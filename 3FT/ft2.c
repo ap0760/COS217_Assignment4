6f7710b4b32af370adbc37df3d116695123bfee7
Ariella Pressner <ap0760@princeton.edu>
2024-04-08 21:41:25 -0400

    create ft2 for scratchwork

---
+++ 3FT/ft2.c
@@ -0,0 +1,164 @@
+#include <stddef.h>
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "a4def.h"
+#include "nodeFT.h"
+#include "ft.h"
+
+
+/*
+  A File Tree is a representation of a hierarchy of directories
+  and files, represented as an AO with 3 state variables:
+*/
+
+/* 1. a flag for being in an initialized state (TRUE) or not (FALSE) */
+static boolean bIsInitialized;
+/* 2. a pointer to the root node in the hierarchy */
+static Node_T oNRoot;
+/* 3. a counter of the number of nodes in the hierarchy */
+static size_t ulCount;
+
+int FT_insertDir(const char *pcPath);
+
+boolean FT_containsDir(const char *pcPath);
+
+int FT_rmDir(const char *pcPath);
+
+int FT_insertFile(const char *pcPath, void *pvContents,
+                  size_t ulLength);
+
+boolean FT_containsFile(const char *pcPath);
+
+int FT_rmFile(const char *pcPath);
+
+/*
+  Traverses the FT to find a node with absolute path pcPath. Returns a
+  int SUCCESS status and sets *poNResult to be the node, if found.
+  Otherwise, sets *poNResult to NULL and returns with status:
+  * INITIALIZATION_ERROR if the DT is not in an initialized state
+  * BAD_PATH if pcPath does not represent a well-formatted path
+  * CONFLICTING_PATH if the root's path is not a prefix of pcPath
+  * NO_SUCH_PATH if no node with pcPath exists in the hierarchy
+  * MEMORY_ERROR if memory could not be allocated to complete request
+ */
+static int FT_findNode(const char *pcPath, Node_T *poNResult) {
+   Path_T oPPath = NULL;
+   Node_T oNFound = NULL;
+   int iStatus;
+
+   assert(pcPath != NULL);
+   assert(poNResult != NULL);
+
+   if(!bIsInitialized) {
+      *poNResult = NULL;
+      return INITIALIZATION_ERROR;
+   }
+
+   iStatus = Path_new(pcPath, &oPPath);
+   if(iStatus != SUCCESS) {
+      *poNResult = NULL;
+      return iStatus;
+   }
+
+   iStatus = DT_traversePath(oPPath, &oNFound);
+   if(iStatus != SUCCESS)
+   {
+      Path_free(oPPath);
+      *poNResult = NULL;
+      return iStatus;
+   }
+
+   if(oNFound == NULL) {
+      Path_free(oPPath);
+      *poNResult = NULL;
+      return NO_SUCH_PATH;
+   }
+
+   if(Path_comparePath(Node_getPath(oNFound), oPPath) != 0) {
+      Path_free(oPPath);
+      *poNResult = NULL;
+      return NO_SUCH_PATH;
+   }
+
+   Path_free(oPPath);
+   *poNResult = oNFound;
+   return SUCCESS;
+}
+
+/*
+  Returns the contents of the file with absolute path pcPath.
+  Returns NULL if unable to complete the request for any reason.
+
+  Note: checking for a non-NULL return is not an appropriate
+  contains check, because the contents of a file may be NULL.
+*/
+void *FT_getFileContents(const char *pcPath)
+{
+    assert (pcPath != NULL);
+    /* check if a node with this name exists in the FT - 
+    need an FT version of the DT_findNode function */
+
+
+}
+
+/*
+  Replaces current contents of the file with absolute path pcPath with
+  the parameter pvNewContents of size ulNewLength bytes.
+  Returns the old contents if successful. (Note: contents may be NULL.)
+  Returns NULL if unable to complete the request for any reason.
+*/
+void *FT_replaceFileContents(const char *pcPath, void *pvNewContents,
+                             size_t ulNewLength);
+
+/*
+  Returns SUCCESS if pcPath exists in the hierarchy,
+  Otherwise, returns:
+  * INITIALIZATION_ERROR if the FT is not in an initialized state
+  * BAD_PATH if pcPath does not represent a well-formatted path
+  * CONFLICTING_PATH if the root's path is not a prefix of pcPath
+  * NO_SUCH_PATH if absolute path pcPath does not exist in the FT
+  * MEMORY_ERROR if memory could not be allocated to complete request
+
+  When returning SUCCESS,
+  if path is a directory: sets *pbIsFile to FALSE, *pulSize unchanged
+  if path is a file: sets *pbIsFile to TRUE, and
+                     sets *pulSize to the length of file's contents
+
+  When returning another status, *pbIsFile and *pulSize are unchanged.
+*/
+int FT_stat(const char *pcPath, boolean *pbIsFile, size_t *pulSize);
+
+/*
+  Sets the FT data structure to an initialized state.
+  The data structure is initially empty.
+  Returns INITIALIZATION_ERROR if already initialized,
+  and SUCCESS otherwise.
+*/
+int FT_init(void);
+
+/*
+  Removes all contents of the data structure and
+  returns it to an uninitialized state.
+  Returns INITIALIZATION_ERROR if not already initialized,
+  and SUCCESS otherwise.
+*/
+int FT_destroy(void);
+
+/*
+  Returns a string representation of the
+  data structure, or NULL if the structure is
+  not initialized or there is an allocation error.
+
+  The representation is depth-first with files
+  before directories at any given level, and nodes
+  of the same type ordered lexicographically.
+
+  Allocates memory for the returned string,
+  which is then owned by client!
+*/
+char *FT_toString(void);
+
+#endif
